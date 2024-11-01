d98e5bd3a9cfc8d56c46ddfab5fbab9ff16a1126
Dominik Baumann <hey@sobrinth.com>
2024-11-01 21:22:42 +0100

    Create a vulkan instance and show a empty window

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,100 @@
 use std::error::Error;
+use std::ffi::CString;
+use ash::{vk, Entry, Instance};
+use winit::application::ApplicationHandler;
+use winit::dpi::PhysicalSize;
+use winit::event::WindowEvent;
+use winit::event_loop::ControlFlow::Poll;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::raw_window_handle::{HasDisplayHandle};
+use winit::window::{Window, WindowId};
 
-struct VulkanApp;
+struct VulkanApp {
+    _entry: Entry,
+    instance: Instance,
+}
 
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+    vulkan: Option<VulkanApp>,
+}
+
+fn main() {
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(Poll);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
+}
 
 impl VulkanApp {
-    fn new() -> Result<Self, Box<dyn Error>> {
-        log::info!("Creating vulkan application");
-        Ok(Self)
+    fn new(window: &Window) -> Self {
+        log::debug!("Creating vulkan application");
+
+        let entry = unsafe { Entry::load().expect("Failed to create entry.") };
+        let instance = Self::create_instance(&entry, window).unwrap();
+
+        Self {
+            _entry: entry,
+            instance,
+        }
     }
 
     fn run(&mut self) {
         log::info!("Running vulkan application");
     }
+
+    fn create_instance(entry: &Entry, window: &Window) -> Result<Instance, Box<dyn Error>> {
+        let app_name = CString::new("Vulkan Application").unwrap();
+        let engine_name = CString::new("No Engine").unwrap();
+        let app_info = vk::ApplicationInfo::default()
+            .application_name(app_name.as_c_str())
+            .application_version(vk::make_api_version(0, 1, 0, 0))
+            .engine_name(engine_name.as_c_str())
+            .engine_version(vk::make_api_version(0, 1, 0, 0))
+            .api_version(vk::make_api_version(0, 1, 0, 0));
+
+        let extension_names = ash_window::enumerate_required_extensions(window.display_handle().unwrap().as_raw())
+            .unwrap().to_vec();
+
+        let instance_create_info = vk::InstanceCreateInfo::default()
+            .application_info(&app_info)
+            .enabled_extension_names(&extension_names);
+
+        unsafe { Ok(entry.create_instance(&instance_create_info, None)?) }
+    }
 }
-fn main() {
-    env_logger::init();
-    match VulkanApp::new() {
-        Ok(mut app) => app.run(),
-        Err(error) => log::error!("Failed to create application. Cause: {}", error),
+impl Drop for VulkanApp {
+    fn drop(&mut self) {
+        unsafe {
+            self.instance.destroy_instance(None);
+        }
+    }
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = event_loop
+            .create_window(
+                Window::default_attributes()
+                    .with_title("Vulkan App with Ash")
+                    .with_inner_size(PhysicalSize::new(800, 600)),
+            )
+            .unwrap();
+
+        self.vulkan = Some(VulkanApp::new(&window));
+        self.window = Some(window);
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            _ => (),
+        }
     }
 }
