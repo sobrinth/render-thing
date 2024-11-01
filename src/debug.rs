f5bbc7175ec54e401e6047bf990084a2d6b37132
Dominik Baumann <hey@sobrinth.com>
2024-11-01 21:59:56 +0100

    Add Vulkan Validation layers

---
+++ src/debug.rs
@@ -0,0 +1,95 @@
+﻿use std::ffi::{c_char, c_void, CStr, CString};
+use ash::{vk, Entry, Instance};
+use ash::ext::debug_utils;
+
+#[cfg(debug_assertions)]
+pub const ENABLE_VALIDATION_LAYERS: bool = true;
+#[cfg(not(debug_assertions))]
+pub const ENABLE_VALIDATION_LAYERS: bool = false;
+
+pub const REQUIRED_LAYERS: [&str; 1] = ["VK_LAYER_KHRONOS_validation"];
+
+unsafe extern "system" fn vulkan_debug_callback(
+    flag: vk::DebugUtilsMessageSeverityFlagsEXT,
+    typ: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _: *mut c_void,
+) -> vk::Bool32 {
+    use vk::DebugUtilsMessageSeverityFlagsEXT as Flag;
+
+    let message = CStr::from_ptr((*p_callback_data).p_message);
+    match flag {
+        Flag::VERBOSE => log::debug!("{:?} - {:?}", typ, message),
+        Flag::INFO => log::info!("{:?} - {:?}", typ, message),
+        Flag::WARNING => log::warn!("{:?} - {:?}", typ, message),
+        _ => log::error!("{:?} - {:?}", typ, message),
+    }
+    vk::FALSE
+}
+/// Get the pointers to the validation layers names.
+/// Also return the corresponding `CString` to avoid dangling pointers.
+pub fn get_layer_names_and_pointers() -> (Vec<CString>, Vec<*const c_char>) {
+    let layer_names = REQUIRED_LAYERS
+        .iter()
+        .map(|n| CString::new(*n).unwrap())
+        .collect::<Vec<_>>();
+    let layer_names_ptrs = layer_names
+        .iter()
+        .map(|n| n.as_ptr())
+        .collect::<Vec<_>>();
+    (layer_names, layer_names_ptrs)
+}
+
+
+/// Check if the required validation set in `REQUIRED_LAYERS`
+/// are supported by the Vulkan instance.
+///
+/// # Panics
+///
+/// Panic if at least one of the layers is not supported.
+pub fn check_validation_layer_support(entry: &Entry) {
+    let supported_layers = unsafe { entry.enumerate_instance_layer_properties().unwrap() };
+    for required in REQUIRED_LAYERS.iter() {
+        let found = supported_layers.iter().any(|l| {
+            let name = unsafe { CStr::from_ptr(l.layer_name.as_ptr()) };
+            let name = name.to_str().expect("Failed to get layer name pointer.");
+            required == &name
+        });
+
+        if !found {
+            panic!("Validation layer not supported: {}", required);
+        }
+    }
+}
+
+/// Set up the debug message if validation layers are enabled
+pub fn setup_debug_messenger(
+    entry: &Entry,
+    instance: &Instance,
+) -> Option<(debug_utils::Instance, vk::DebugUtilsMessengerEXT)> {
+    if !ENABLE_VALIDATION_LAYERS {
+        return None;
+    }
+
+    let create_info = vk::DebugUtilsMessengerCreateInfoEXT::default()
+        .flags(vk::DebugUtilsMessengerCreateFlagsEXT::empty())
+        .message_severity(
+            vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
+                | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                | vk::DebugUtilsMessageSeverityFlagsEXT::INFO,
+        )
+        .message_type(
+            vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+                | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+        )
+        .pfn_user_callback(Some(vulkan_debug_callback));
+    let debug_utils = debug_utils::Instance::new(entry, instance);
+    let debug_utils_messenger = unsafe {
+        debug_utils
+            .create_debug_utils_messenger(&create_info, None)
+            .unwrap()
+    };
+
+    Some((debug_utils, debug_utils_messenger))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+mod debug;
+
 use std::error::Error;
 use std::ffi::CString;
 use ash::{vk, Entry, Instance};
+use ash::ext::debug_utils;
 use winit::application::ApplicationHandler;
 use winit::dpi::PhysicalSize;
 use winit::event::WindowEvent;
@@ -8,10 +11,12 @@ use winit::event_loop::ControlFlow::Poll;
 use winit::event_loop::{ActiveEventLoop, EventLoop};
 use winit::raw_window_handle::{HasDisplayHandle};
 use winit::window::{Window, WindowId};
+use crate::debug::{check_validation_layer_support, get_layer_names_and_pointers, setup_debug_messenger, ENABLE_VALIDATION_LAYERS};
 
 struct VulkanApp {
     _entry: Entry,
     instance: Instance,
+    debug_report_callback: Option<(debug_utils::Instance, vk::DebugUtilsMessengerEXT)>
 }
 
 #[derive(Default)]
@@ -36,10 +41,12 @@ impl VulkanApp {
 
         let entry = unsafe { Entry::load().expect("Failed to create entry.") };
         let instance = Self::create_instance(&entry, window).unwrap();
+        let debug_report_callback = setup_debug_messenger(&entry, &instance);
 
         Self {
             _entry: entry,
             instance,
+            debug_report_callback,
         }
     }
 
@@ -57,12 +64,24 @@ impl VulkanApp {
             .engine_version(vk::make_api_version(0, 1, 0, 0))
             .api_version(vk::make_api_version(0, 1, 0, 0));
 
-        let extension_names = ash_window::enumerate_required_extensions(window.display_handle().unwrap().as_raw())
+        let mut extension_names = ash_window::enumerate_required_extensions(window.display_handle().unwrap().as_raw())
             .unwrap().to_vec();
 
-        let instance_create_info = vk::InstanceCreateInfo::default()
+        if ENABLE_VALIDATION_LAYERS {
+            extension_names.push(debug_utils::NAME.as_ptr());
+        }
+
+        let (_layer_names, layer_names_ptrs) = get_layer_names_and_pointers();
+
+
+        let mut instance_create_info = vk::InstanceCreateInfo::default()
             .application_info(&app_info)
             .enabled_extension_names(&extension_names);
+
+        if ENABLE_VALIDATION_LAYERS {
+            check_validation_layer_support(entry);
+            instance_create_info = instance_create_info.enabled_layer_names(&layer_names_ptrs);
+        }
 
         unsafe { Ok(entry.create_instance(&instance_create_info, None)?) }
     }
@@ -70,6 +89,9 @@ impl VulkanApp {
 impl Drop for VulkanApp {
     fn drop(&mut self) {
         unsafe {
+            if let Some((report, callback)) = self.debug_report_callback.take() {
+                report.destroy_debug_utils_messenger(callback, None);
+            }
             self.instance.destroy_instance(None);
         }
     }
