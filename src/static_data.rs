10c51af18cb6f1f464e8b3d83c09fe482a3f8b1e
Dominik Baumann <hey@sobrinth.com>
2024-11-04 23:29:36 +0100

    Map texture kinda scuffed onto thing. Not fixing, because it will be gone soon

--- src/static_data.rs
+++ src/static_data.rs
@@ -1,22 +1,25 @@
 ﻿use crate::primitives::Vertex;
 
-pub(crate) const VERTEX_SIZE: usize = 20;
 pub(crate) const VERTICES: [Vertex; 4] = [
     Vertex {
         pos: [-0.5, -0.5],
         color: [1.0, 0.0, 0.0],
+        coords: [0.0, 0.0],
     },
     Vertex {
         pos: [0.5, -0.5],
         color: [0.0, 1.0, 0.0],
+        coords: [1.0, 0.0],
     },
     Vertex {
         pos: [0.5, 0.5],
         color: [0.0, 0.0, 1.0],
+        coords: [1.0, 1.0],
     },
     Vertex {
         pos: [-0.5, 0.5],
         color: [1.0, 1.0, 1.0],
+        coords: [0.0, 1.0],
     },
 ];
 pub(crate) const INDICES: [u16; 6] = [0, 1, 2, 2, 3, 0];
