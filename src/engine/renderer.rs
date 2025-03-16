5dd005c9b5d60f1647dbbcfa1c2ef476b681ad2c
Dominik Baumann <hey@sobrinth.com>
2025-03-16 20:48:59 +0100

    Move swapchain creation to new implementation

--- src/engine/renderer.rs
+++ src/engine/renderer.rs
@@ -1,17 +1,33 @@
 use crate::engine::context::VkContext;
+use crate::engine::swapchain::Swapchain;
 use winit::window::Window;
 
 pub struct VulkanRenderer {
     pub context: VkContext,
+
+    swapchain: Swapchain,
 }
 
 impl VulkanRenderer {
     pub fn initialize(window: &Window) -> Self {
-        log::debug!("Creating vulkan context");
-
         let context = VkContext::initialize(window);
-        Self {
-            context,
-        }
+
+        let swapchain = Swapchain::create(
+            &context,
+            [window.inner_size().width, window.inner_size().height],
+        );
+        Self { context, swapchain }
+    }
+
+    pub fn wait_gpu_idle(&self) {
+        unsafe { self.context.device.device_wait_idle() }.unwrap();
+    }
+}
+
+impl Drop for VulkanRenderer {
+    fn drop(&mut self) {
+        log::debug!("Start: Dropping renderer");
+        unsafe { self.swapchain.destroy(&self.context.device) }
+        log::debug!("End: Dropping renderer");
     }
 }
